12894b9502b2d9a1393eaa3d67954cbaca292afc
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-11-13 23:30:42 -0500

    Add Script for Outputting Frame Indices per timestamp

--- scripts/save_spatio_temporal_clip_features.py
+++ scripts/save_spatio_temporal_clip_features.py
@@ -4,6 +4,8 @@ from sys import stdout as sys_stdout
 from math import ceil as math_ceil
 from pickle import dump as pickle_dump
 from argparse import ArgumentParser
+from json import load as json_load, dump as json_dump
+from collections import defaultdict
 import numpy as np
 from torch import (
     zeros as torch_zeros,
@@ -27,19 +29,22 @@ environ['DECORD_EOF_RETRY_MAX'] = '20480'
 # Can't process /home/zhanwen/siq2/siq2/video/n6-ef_YHeJU.mp4
 
 
-def save_features(video_clip_features, clip_feat_path):
+def save_features(video_clip_features, clip_feat_path, ts_by_video, ts_by_videol_fpath):
     for key, features in video_clip_features.items():
         with open(f"{clip_feat_path}/{key}.pkl", 'wb') as f:
             pickle_dump(features, f)
 
+    with open(ts_by_videol_fpath, 'w') as f:
+        json_dump(ts_by_video, f)
 
-def load_video(vis_path, device, num_frm=100):
+
+def load_video(vis_path, device, cuts, num_frm=100):
     with open(vis_path, 'rb') as file_in:
         # vr = VideoReader(file_in, ctx=gpu(0), num_threads=0)
         vr = VideoReader(file_in, ctx=gpu(0), num_threads=0)
     total_frame_num = len(vr)
-    total_num_frm = min(total_frame_num, num_frm)
-    frame_idx = get_seq_frames(total_frame_num, total_num_frm)
+    total_num_frm = min(total_frame_num, num_frm, cuts)
+    frame_idx, ts_to_frames = get_seq_frames(total_frame_num, total_num_frm, cuts)
     # img_array = vr.get_batch(frame_idx).asnumpy()  # (n_clips*num_frm, H, W, 3)
     img_array = vr.get_batch(frame_idx) # (n_clips*num_frm, H, W, 3)
     del vr
@@ -56,18 +61,42 @@ def load_video(vis_path, device, num_frm=100):
     #     clip_imgs.append(Image_fromarray(img_array[0, j]))
 
     clip_imgs = [Image_fromarray(img_array[0, j]) for j in range(total_num_frm)]
-    return clip_imgs
+    return clip_imgs, ts_to_frames
 
 
-def get_seq_frames(total_num_frames, desired_num_frames):
+def get_seq_frames(total_num_frames, desired_num_frames, cuts: list[str]):
+    # Even selection.
+    cuts = sorted(cuts)
+    cuts_float_start = []
+    cuts_float_end = []
+    for cut in cuts:
+        start_str, end_str = cut.split('-')
+        start_float, end_float = float(start_str), float(end_str)
+        cuts_float_start.append(start_float)
+        cuts_float_end.append(end_float)
+    cuts_float_end = sorted(cuts_float_end)
+
+    summy = sum(cuts_float_end)
+
+    percentiles = [cut_float/summy for cut_float in cuts_float_end]
+    frame_counts = [total_num_frames * percentile for percentile in percentiles]
+
     seg_size = float(total_num_frames - 1) / desired_num_frames
     seq = []
+    ts_out = defaultdict(list)
+    frame_counts_index = 0
     for i in range(desired_num_frames):
         start = int(np.round(seg_size * i))
         end = int(np.round(seg_size * (i + 1)))
-        seq.append((start + end) // 2)
+        frame_idx = (start + end) // 2
+        frame_threshold = frame_counts[frame_counts_index]
+        if frame_idx >= frame_threshold:
+            frame_idx += 1
+        ts_subvideo = cuts[frame_counts_index]
+        ts_out[ts_subvideo].append(frame_idx)
+        seq.append(frame_idx)
 
-    return seq
+    return seq, ts_out
 
 
 def get_spatio_temporal_features(features, num_temporal_tokens=100):
@@ -92,6 +121,9 @@ def get_spatio_temporal_features(features, num_temporal_tokens=100):
 def parse_args():
     parser = ArgumentParser(description="Training")
 
+    parser.add_argument("--ts_by_videol_fpath", required=True,
+                        help="Path to read the qa file from.")
+    parser.add_argument("--qa_path", required=True, help="Path to read the qa file from.")
     parser.add_argument("--video_dir_path", required=True, help="Path to read the videos from.")
     parser.add_argument("--clip_feat_path", required=True, help="The output dir to save the features in.")
     parser.add_argument("--infer_batch", required=False, type=int, default=32,
@@ -102,11 +134,32 @@ def parse_args():
     return args
 
 
+def get_unique_videos(qas):
+    qas_by_video = defaultdict(list)
+    for qa in qas:
+        qas_by_video[qa['video_id']].append(qa)
+    return qas_by_video
+
+
+def get_unique_ts(qas):
+    qas_by_video = defaultdict(set)
+    for qa in qas:
+        qas_by_video[qa['video_id']].add(qa['ts'])
+    return qas_by_video
+
+
+
 def main():
     args = parse_args()
+    ts_by_videol_fpath = args.ts_by_videol_fpath
+    qa_path = args.qa_path
     video_dir_path = args.video_dir_path
     clip_feat_path = args.clip_feat_path
     infer_batch = args.infer_batch
+    with open(qa_path, 'rb') as file_in:
+        qas = json_load(file_in)
+
+    # qas_by_video = get_unique_videos(qas)
     os_makedirs(clip_feat_path, exist_ok=True)
     device = torch_device('cuda:0')
     non_blocking = False
@@ -122,14 +175,20 @@ def main():
     counter = 0
     image_processor_preprocess = image_processor.preprocess
     select_hidden_state_layer = -2
+
+    cuts_by_video = get_unique_ts(qas)
+    ts_by_video = defaultdict(lambda: defaultdict(list))  # vid: {ts: indices}
+
     for video_name in tqdm(all_videos, file=sys_stdout):
+        cuts = cuts_by_video[video_name]
         print(f'Started processing {video_name}')
         video_path = f"{video_dir_path}/{video_name}"
         video_id = video_name.split('.')[0]
         if os_path_exists(f"{clip_feat_path}/{video_id}.pkl"):  # Check if the file is already processed
             continue
         try:
-            video = load_video(video_path, device, num_frm=100)
+            video, ts_out = load_video(video_path, device, cuts, num_frm=100)
+            ts_by_video[video_name] = ts_out
             video_tensor = image_processor_preprocess(video, return_tensors='pt')['pixel_values'].to(torch_float16, non_blocking=non_blocking)
 
             n_chunk = len(video_tensor)
@@ -154,10 +213,12 @@ def main():
             raise e
 
         if counter % 512 == 0:  # Save after every 512 videos, update this number as per your requirements
-            save_features(video_clip_features, clip_feat_path)
+            save_features(video_clip_features,
+                          clip_feat_path, ts_by_video, ts_by_videol_fpath)
             video_clip_features = {}
 
-    save_features(video_clip_features, clip_feat_path)
+    save_features(video_clip_features, clip_feat_path,
+                  ts_by_video, ts_by_videol_fpath)
 
 
 if __name__ == "__main__":
