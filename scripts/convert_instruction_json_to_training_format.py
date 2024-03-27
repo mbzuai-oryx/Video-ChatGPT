f27bf8c29b77efcc2ca07e398e92aa1de09f5063
Muhammad Maaz <mmaaz60@gmail.com>
2024-03-28 00:47:14 +0400

    Update convert_instruction_json_to_training_format.py

--- scripts/convert_instruction_json_to_training_format.py
+++ scripts/convert_instruction_json_to_training_format.py
@@ -1,3 +1,4 @@
+import os
 import json
 import argparse
 
@@ -5,8 +6,12 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Training")
 
-    parser.add_argument("--input_json_file", required=True, help="")
-    parser.add_argument("--output_json_file", required=True, help="")
+    parser.add_argument("--input_json_file", required=True,
+                        help="Path to input json file (i.e. VideoInstruct_Dataset.json)")
+    parser.add_argument("--output_json_file", required=True,
+                        help="Path to output json file (i.e. VideoInstruct_Dataset_Train.json)")
+    parser.add_argument("--clip_feature_path", required=False, default="",
+                        help="Path to generated CLIP feature paths to filter any missing video ids (optional).")
 
     args = parser.parse_args()
 
@@ -17,19 +22,34 @@ def main():
     args = parse_args()
     input_json_file = args.input_json_file
     output_json_file = args.output_json_file
+    clip_feature_path = args.clip_feature_path
+
+    clip_features_files_witout_extension = ""
+    if clip_feature_path:
+        clip_features_files = os.listdir(clip_feature_path)
+        clip_features_files_witout_extension = []
+        for file in clip_features_files:
+            clip_features_files_witout_extension.append(file.split('.')[0])
+
 
     input_json_contents = json.load(open(input_json_file, 'r'))
     output_json_contents = []
     for i, content in enumerate(input_json_contents):
-        output_content = {'id': content['video_id'], 'video': f"{content['video_id']}.pkl", 'conversations': []}
+        valid = False
+        if not clip_feature_path:
+            valid = True
+        elif content['video_id'] in clip_features_files_witout_extension:
+            valid = True
 
-        if i % 2 == 0:
-            output_content['conversations'].append({'from': 'human', 'value': f"{content['q']}\n<video>"})
-        else:
-            output_content['conversations'].append({'from': 'human', 'value': f"<video>\n{content['q']}"})
-
-        output_content['conversations'].append({'from': 'gpt', 'value': content['a']})
-        output_json_contents.append(output_content)
+        if valid:
+            output_content = {'id': content['video_id'], 'video': f"{content['video_id']}.pkl", 'conversations': []}
+            # This is critical
+            if i % 2 == 0:
+                output_content['conversations'].append({'from': 'human', 'value': f"{content['q']}\n<video>"})
+            else:
+                output_content['conversations'].append({'from': 'human', 'value': f"<video>\n{content['q']}"})
+            output_content['conversations'].append({'from': 'gpt', 'value': content['a']})
+            output_json_contents.append(output_content)
 
     print(f"Total annotations retained: {len(output_json_contents)}")
     with open(output_json_file, 'w') as f:
